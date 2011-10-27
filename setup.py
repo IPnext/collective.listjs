dfb383fc7f9b6a7a882589e5d48322d3ef69f4e0
Rigel Di Scala <zedr@zedr.com>
2011-10-27 15:38:55 +0300

    Fixed broken develop script

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.1.2'
 setup(name='collective.listjs',
       version=version,
       description="Plone integration for Listjs (http://www.listjs.com)",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
